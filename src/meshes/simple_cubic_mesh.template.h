1fa99f2e8e9627b71824751645d3ffd3a951050c
wobblygomboc <chrisvaquero@gmail.com>
2022-05-15 19:40:20 +0000

    Deploying to gh-pages from  @ c03c48e58aac8da1b2868a503126743393ed1860 🚀

--- src/meshes/simple_cubic_mesh.template.h
+++ src/meshes/simple_cubic_mesh.template.h
@@ -3,7 +3,7 @@
 // LIC// multi-physics finite-element library, available
 // LIC// at http://www.oomph-lib.org.
 // LIC//
-// LIC// Copyright (C) 2006-2021 Matthias Heil and Andrew Hazel
+// LIC// Copyright (C) 2006-2022 Matthias Heil and Andrew Hazel
 // LIC//
 // LIC// This library is free software; you can redistribute it and/or
 // LIC// modify it under the terms of the GNU Lesser General Public
@@ -119,7 +119,7 @@ namespace oomph
     /// Access function for number of elements in y directions
     const unsigned& nz() const
     {
-      return Nx;
+      return Nz;
     }
 
   protected:
